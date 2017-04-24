d0c2981e80577d6fdb02a13ac78b3a802a349a0d
Carmel Cohen <carmelc@wix.com>
2017-04-24 18:21:51 +0300

    Support tslint 3 (#3)
    * dev(TSLint rules): no-jasmine-focus support tslint 3
    
    * dev(TSLint rules): no-jasmine-focus support tslint 3
    
    * dev(TSLint rules): no-jasmine-focus support tslint 3
    
    * dev(TSLint rules): no-jasmine-focus support tslint 3

--- rules/noJasmineFocusRule.js
+++ rules/noJasmineFocusRule.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const Lint = require('tslint');
+let Lint = require('tslint');
+if (!Lint.Rules) {
+  //tslint 3
+  Lint = require('tslint/lib/lint');
+}
 
 const PROHIBITED_WORDS = ['fit', 'fdescribe'];
 const REGEX = new RegExp(`^(${PROHIBITED_WORDS.join('|')})$`);
 
 class Rule extends Lint.Rules.AbstractRule {
-  constructor(options) {
-    super(options);
-  }
-
   apply(sourceFile) {
     return this.applyWithWalker(new NoJasmineFocusWalker(sourceFile, this.getOptions()));
   }
@@ -25,6 +25,13 @@ class NoJasmineFocusWalker extends Lint.RuleWalker {
 
     super.visitCallExpression(node);
   }
+}
+
+if (!Lint.RuleWalker.prototype.addFailureAt) {
+  //tslint 3
+  NoJasmineFocusWalker.prototype.addFailureAt = function (nodeStart, length, msg) {
+    return this.addFailure(this.createFailure(nodeStart, length, msg));
+  };
 }
 
 exports.Rule = Rule;
