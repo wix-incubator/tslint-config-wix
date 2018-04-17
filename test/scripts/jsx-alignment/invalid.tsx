157a434ae53c4a9ace4f30bf985c53c477fff00e
Lior Belinsky <liorbe@liorbe-mbp15-2.home>
2018-04-17 20:27:10 +0300

    jsx-alignment

---
+++ test/scripts/jsx-alignment/invalid.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Comp: React.SFC = () => (
+  <input
+    readOnly
+    value="1"
+    />
+);
+
+const Comp2: React.SFC = () => (
+  <input
+    readOnly
+    value="1"/>
+);
---
+++ test/scripts/jsx-alignment/valid.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const Comp: React.SFC = () => (
+  <input
+    readOnly
+    value="1"
+  />
+);
